c9576856c43eacd36062432dfca718a8e4e9d38c
Asadullah-Imran <asadullahimran19@gmail.com>
2024-08-15 00:08:22 +0600

    bla

--- api/index.js
+++ api/index.js
@@ -11,15 +11,18 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
-
 // Define routes
 app.use("/api/auth", authRoutes);
 app.use("/api/test", testRoutes);
 app.get("/", (req, res) => res.send("Hello World"));
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  });
