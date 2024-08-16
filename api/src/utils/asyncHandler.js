00a54038d6e2655ecbb4308a0b2edccc11311850
Asadullah-Imran <asadullahimran19@gmail.com>
2024-08-16 14:27:46 +0600

    created asyncHandler.js for handle all async operation

---
+++ api/src/utils/asyncHandler.js
@@ -0,0 +1,14 @@
+// this utils file is created to handle async functions in a better way
+// instead of using try catch block in every async function
+// we can use this asyncHandler to wrap the async function and it will handle the error for us
+// and pass it to the global error handler middleware
+
+const asyncHandler = (fn) => {
+  return async (req, res, next) => {
+    try {
+      await fn(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+};
