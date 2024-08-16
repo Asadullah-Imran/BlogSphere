5aabe0ca7990483549776b5932252524a32029eb
Asadullah-Imran <asadullahimran19@gmail.com>
2024-08-16 18:03:25 +0600

    vercel.json modified

---
+++ api/index.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+import app from "./src/app.js";
+import connectDB from "./src/db/db.js";
+
+// Configure dotenv
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.log("MongoDB connection error in index.js:", err));
