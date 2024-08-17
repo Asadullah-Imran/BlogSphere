bdee8ec0edf307026e810f26c711b7d341089b19
Asadullah-Imran <asadullahimran19@gmail.com>
2024-08-17 14:37:06 +0600

    chore: Update cookie options in login controller to use "none" for sameSite attribute

--- api/src/controllers/auth.controllers.js
+++ api/src/controllers/auth.controllers.js
@@ -182,13 +182,13 @@ export const login = asyncHandler(async (req, res) => {
     .cookie("accessToken", accessToken, {
       httpOnly: true,
       secure: true, // Ensure this is true in production
-      sameSite: "lax", // or "strict" depending on your needs
+      sameSite: "none", // or "strict" depending on your needs
       maxAge: accessTokenExpiry, // Set cookie expiration to 1 day
     })
     .cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: true, // Ensure this is true in production
-      sameSite: "lax", // or "strict" depending on your needs
+      sameSite: "none", // or "strict" depending on your needs
       maxAge: refreshTokenExpiry, // Set cookie expiration to 10 days
     })
     .json(new ApiResponse(200, loggedInUser, "User logged in successfully"));
