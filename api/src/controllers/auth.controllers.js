932a5f3da3fccffd8cc4e5580a59c2112261ee88
Asadullah-Imran <asadullahimran19@gmail.com>
2024-08-17 21:54:46 +0600

    chore: Update cookie options in login controller to use "lax" for sameSite attribute

--- api/src/controllers/auth.controllers.js
+++ api/src/controllers/auth.controllers.js
@@ -183,14 +183,16 @@ export const login = asyncHandler(async (req, res) => {
     .cookie("accessToken", accessToken, {
       httpOnly: true, //
       secure: true, // Ensure this is true in production
-      sameSite: "lax", // or "strict" depending on your needs
+      // sameSite: "lax", // or "strict" depending on your needs
       maxAge: accessTokenExpiry, // Set cookie expiration to 1 day
+      // domain: "vercel.app", // Set the correct domain
     })
     .cookie("refreshToken", refreshToken, {
       httpOnly: true, //
       secure: true, // Ensure this is true in production
       sameSite: "lax", // or "strict" depending on your needs
       maxAge: refreshTokenExpiry, // Set cookie expiration to 10 days
+      domain: "vercel.app", // Set the correct domain
     })
     .json(new ApiResponse(200, loggedInUser, "User logged in successfully"));
 });
