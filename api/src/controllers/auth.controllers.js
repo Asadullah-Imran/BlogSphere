85690a0531a1665a4f2542eddafa229d16627e66
Asadullah-Imran <asadullahimran19@gmail.com>
2024-08-18 14:03:48 +0600

    Refactor logout controller to use tokenOption function for clearing cookies

--- api/src/controllers/auth.controllers.js
+++ api/src/controllers/auth.controllers.js
@@ -220,7 +220,7 @@ export const logout = asyncHandler(async (req, res) => {
   return (
     res
       .status(200)
-      .cookie("Token", "salam vai", tokenOption(accessTokenExpiry))
+      .cookie("Token", "salam vai", options)
       // .clearCookie("accessToken", options) // Setting maxAge to 0 clears the cookie
       // .clearCookie("refreshToken", options)
       .json(new ApiResponse(200, {}, "User logged out successfully"))
