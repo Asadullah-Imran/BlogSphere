45b5d7beac0b9ed5b063b448c805934c2c4e97dc
Asadullah-Imran <asadullahimran19@gmail.com>
2024-08-18 14:37:34 +0600

    Refactor auth middleware to use accessToken instead of access_token

--- api/src/middlewares/auth.middleware.js
+++ api/src/middlewares/auth.middleware.js
@@ -6,7 +6,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 export const verifyJWT = asyncHandler(async (req, res, next) => {
   try {
     const token =
-      req.cookies?.access_token ||
+      req.cookies?.accessToken ||
       req.headers["authorization"]?.replace("Bearer ", "");
     if (!token) {
       throw new ApiError(401, "Unauthorized request");
