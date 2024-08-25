18483dfa286ccbb22370ebdb3769ec1dca3897fd
Asadullah-Imran <asadullahimran19@gmail.com>
2024-08-25 23:02:59 +0600

    feat: Update SinglePost page layout and components
    The SinglePost page layout and components have been updated to improve the user experience. The changes include:
    - Adjusting the main post content width to 3/4 of the container
    - Removing the You May Like section from the right side and placing it on the left side
    - Adding lazy loading for images to improve page load performance
    - Updating the Confirmation Modal component to use the isOpen prop instead of a separate state variable
    
    These changes enhance the readability and usability of the SinglePost page.

--- client/src/pages/SinglePost.jsx
+++ client/src/pages/SinglePost.jsx
@@ -128,11 +128,11 @@ const SinglePost = () => {
   const reactionCount = reactions.length;
 
   return (
-    <div className="max-w-7xl mx-auto p-6 bg-cusLightBG rounded-lg shadow-lg flex flex-col gap-6 md:flex-row">
+    <div className="max-w-7xl mx-auto p-6 bg-cusLightBG rounded-lg shadow-lg flex gap-6">
       {post && (
         <>
           {/* Main Post Content */}
-          <div className="w-full md:w-3/4 order-2 md:order-1">
+          <div className="w-3/4">
             <h1 className="text-5xl font-extrabold mb-6 text-center text-cusPrimaryColor">
               {post.title}
             </h1>
@@ -258,168 +258,20 @@ const SinglePost = () => {
               </button>
             </div>
           </div>
-          {/* You May Like Section */}
-          <div className="w-full md:w-1/4 order-1 md:order-2">
-            <YouMayLike />
+          {/* You May Like Component on the Left */}
+          <div className="w-1/4">
+            <YouMayLike currentPostId={id} tags={post.tags} />
           </div>
         </>
       )}
-
       {/* Confirmation Modal */}
-      {isModalOpen && (
-        <ConfirmationModal
-          onConfirm={handleDeleteComment}
-          onCancel={closeModal}
-        />
-      )}
+      <ConfirmationModal
+        isOpen={isModalOpen}
+        onConfirm={handleDeleteComment}
+        onCancel={closeModal}
+      />
     </div>
   );
-
-  // return (
-  //   <div className="max-w-7xl mx-auto p-6 bg-cusLightBG rounded-lg shadow-lg flex gap-6">
-  //     {post && (
-  //       <>
-  //         {/* Main Post Content */}
-  //         <div className="w-3/4">
-  //           <h1 className="text-5xl font-extrabold mb-6 text-center text-cusPrimaryColor">
-  //             {post.title}
-  //           </h1>
-  //           <img
-  //             src={post.image}
-  //             alt={post.title}
-  //             className="w-full h-auto rounded-lg shadow-lg mb-6"
-  //           />
-  //           <div className="flex items-center gap-4 mb-6">
-  //             <img
-  //               src={post.author.profilePic}
-  //               alt={post.author.fullname}
-  //               className="w-12 h-12 rounded-full"
-  //             />
-  //             <div>
-  //               <p className="text-lg font-semibold text-cusPrimaryColor">
-  //                 {post.author.fullname}
-  //               </p>
-  //               <p className="text-sm text-gray-600">
-  //                 {formatDistanceToNow(new Date(post.createdAt), {
-  //                   addSuffix: true,
-  //                 })}
-  //               </p>
-  //             </div>
-  //           </div>
-  //           <p className="text-lg leading-relaxed text-gray-700 mb-8">
-  //             {post.content}
-  //           </p>
-  //           <div className="flex items-center gap-4 mb-8">
-  //             <button
-  //               onClick={handleAddOrRemoveReaction}
-  //               className="flex items-center text-3xl p-2 rounded-lg hover:bg-red-100 transition duration-300"
-  //             >
-  //               <FaHeart
-  //                 className={`transition duration-300 ${
-  //                   hasReacted ? "text-red-500" : "text-gray-400"
-  //                 }`}
-  //               />
-  //               <span className="ml-2 text-lg font-semibold">
-  //                 {reactionCount}
-  //               </span>
-  //             </button>
-  //           </div>
-  //           <h2 className="text-3xl font-semibold mb-4 text-cusPrimaryColor">
-  //             Comments
-  //           </h2>
-  //           <ul className="space-y-6 mb-8">
-  //             {comments.map((comment) => (
-  //               <li
-  //                 key={comment._id}
-  //                 className="bg-white p-6 rounded-lg shadow-md flex flex-col gap-4"
-  //               >
-  //                 {editCommentId === comment._id ? (
-  //                   <>
-  //                     <textarea
-  //                       value={editedComment}
-  //                       onChange={(e) => setEditedComment(e.target.value)}
-  //                       className="w-full p-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-cusPrimaryColor"
-  //                       rows="3"
-  //                     />
-  //                     <div className="flex justify-end gap-4">
-  //                       <button
-  //                         onClick={handleUpdateComment}
-  //                         className="bg-cusPrimaryColor text-white px-4 py-2 rounded-lg hover:bg-cusSecondaryColor transition duration-300"
-  //                       >
-  //                         Update
-  //                       </button>
-  //                       <button
-  //                         onClick={() => {
-  //                           setEditCommentId(null);
-  //                           setEditedComment("");
-  //                         }}
-  //                         className="bg-gray-300 text-gray-700 px-4 py-2 rounded-lg hover:bg-gray-400 transition duration-300"
-  //                       >
-  //                         Cancel
-  //                       </button>
-  //                     </div>
-  //                   </>
-  //                 ) : (
-  //                   <>
-  //                     <div className="flex justify-between items-start">
-  //                       <div className="flex flex-col gap-2">
-  //                         <p className="text-gray-800 text-lg">
-  //                           {comment.content}
-  //                         </p>
-  //                         <p className="text-sm text-gray-500">
-  //                           - {comment.author.fullname}
-  //                         </p>
-  //                       </div>
-  //                       {user && comment.author._id === user._id && (
-  //                         <div className="flex gap-2 text-gray-500">
-  //                           <FaEdit
-  //                             className="cursor-pointer hover:text-cusPrimaryColor"
-  //                             onClick={() =>
-  //                               handleEditComment(comment._id, comment.content)
-  //                             }
-  //                           />
-  //                           <FaTrashAlt
-  //                             className="cursor-pointer hover:text-red-500"
-  //                             onClick={() => openModal(comment._id)}
-  //                           />
-  //                         </div>
-  //                       )}
-  //                     </div>
-  //                   </>
-  //                 )}
-  //               </li>
-  //             ))}
-  //           </ul>
-  //           <div className="bg-white p-6 rounded-lg shadow-md">
-  //             <textarea
-  //               value={newComment}
-  //               onChange={(e) => setNewComment(e.target.value)}
-  //               className="w-full p-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-cusPrimaryColor"
-  //               rows="3"
-  //               placeholder="Add a comment..."
-  //             />
-  //             <button
-  //               onClick={handleAddComment}
-  //               className="w-full bg-cusPrimaryColor text-white px-4 py-2 rounded-lg mt-4 hover:bg-cusSecondaryColor transition duration-300"
-  //             >
-  //               Add Comment
-  //             </button>
-  //           </div>
-  //         </div>
-  //         {/* You May Like Component on the Left */}
-  //         <div className="w-1/4">
-  //           <YouMayLike currentPostId={id} tags={post.tags} />
-  //         </div>
-  //       </>
-  //     )}
-  //     {/* Confirmation Modal */}
-  //     <ConfirmationModal
-  //       isOpen={isModalOpen}
-  //       onConfirm={handleDeleteComment}
-  //       onCancel={closeModal}
-  //     />
-  //   </div>
-  // );
 };
 
 export default SinglePost;
