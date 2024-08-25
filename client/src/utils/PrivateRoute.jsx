340c00140ab01cab46eedec847775ca6f0c6b3af
Asadullah-Imran <asadullahimran19@gmail.com>
2024-08-25 22:45:01 +0600

    feat: Update authContext import in PrivateRoute.jsx

--- client/src/utils/PrivateRoute.jsx
+++ client/src/utils/PrivateRoute.jsx
@@ -1,7 +1,7 @@
 // src/utils/PrivateRoute.jsx
 import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext } from "../context/authContext";
 
 const PrivateRoute = ({ element, ...rest }) => {
   const { user } = useContext(AuthContext);
