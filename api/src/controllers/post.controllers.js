2d095b04e22570207f25766589a8d08eee5a8215
Asadullah-Imran <asadullahimran19@gmail.com>
2024-08-27 00:32:47 +0600

    dsfs

--- api/src/controllers/post.controllers.js
+++ api/src/controllers/post.controllers.js
@@ -43,46 +43,49 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 //   }
 // };
 
-export const createPost = asyncHandler(async (req, res, next) => {
-  const { title, content, tags } = req.body;
-  const author = "66c8e298d5bdf18dacdcd7c8"; // Replace with dynamic author ID
-  // const author = req.user._id; // Assuming user info is available in req.user
-  // Parse the tags JSON string back into an array
-  let parsedTags = [];
-  try {
-    parsedTags = JSON.parse(tags);
-  } catch (error) {
-    throw new ApiError(400, "Invalid tags format");
-  }
+export const createPost = asyncHandler(async (req, res) => {
+  // const { title, content, tags } = req.body;
+  // const author = "66c8e298d5bdf18dacdcd7c8"; // Replace with dynamic author ID
+  // // const author = req.user._id; // Assuming user info is available in req.user
+  // // Parse the tags JSON string back into an array
+  // let parsedTags = [];
+  // try {
+  //   parsedTags = JSON.parse(tags);
+  // } catch (error) {
+  //   throw new ApiError(400, "Invalid tags format");
+  // }
 
-  let image;
-  if (req.file && req.file.path) {
-    const uploadResponse = await uploadOnCloudinary(req.file.path);
-    if (!uploadResponse) {
-      throw new ApiError(500, "Failed to upload image");
-    }
-    image = uploadResponse.secure_url; // Save only the secure URL or other required field
-  } else {
-    throw new ApiError(408, "Image is required");
-  }
+  // let image;
+  // if (req.file && req.file.path) {
+  //   const uploadResponse = await uploadOnCloudinary(req.file.path);
+  //   if (!uploadResponse) {
+  //     throw new ApiError(500, "Failed to upload image");
+  //   }
+  //   image = uploadResponse.secure_url; // Save only the secure URL or other required field
+  // } else {
+  //   throw new ApiError(408, "Image is required");
+  // }
 
-  const post = new Post({
-    title,
-    content,
-    image, // Save the secure URL of the uploaded image
-    tags: parsedTags,
-    author,
-  });
+  // const post = new Post({
+  //   title,
+  //   content,
+  //   image, // Save the secure URL of the uploaded image
+  //   tags: parsedTags,
+  //   author,
+  // });
 
-  if (!post) {
-    throw new ApiError(400, "Failed to create post");
-  }
+  // if (!post) {
+  //   throw new ApiError(400, "Failed to create post");
+  // }
 
-  await post.save();
-  res.status(201).json({
-    success: true,
-    data: post,
-  });
+  // await post.save();
+
+  // res.status(201).json({
+  //   success: true,
+  //   data: post,
+  // });
+
+  res.status(201).json(new ApiResponse(201, {}, "Post created successfully"));
 });
 
 // Get all posts
