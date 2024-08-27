dd2de909efdd3e53fc8145cd0c8ea1b8b5127f5b
Asadullah-Imran <asadullahimran19@gmail.com>
2024-08-28 01:29:29 +0600

    Refactor createPost function to use asyncHandler middleware

--- api/src/controllers/post.controllers.js
+++ api/src/controllers/post.controllers.js
@@ -45,6 +45,7 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 export const createPost = asyncHandler(async (req, res) => {
   const { title, content } = req.body;
+
   const author = "66c8e298d5bdf18dacdcd7c8"; // Replace with dynamic author ID
   // const author = req.user._id; // Assuming user info is available in req.user
   // Parse the tags JSON string back into an array
@@ -55,24 +56,29 @@ export const createPost = asyncHandler(async (req, res) => {
   //   throw new ApiError(400, "Invalid tags format");
   // }
 
-  // let image;
-  // if (req.file && req.file.path) {
-  //   const uploadResponse = await uploadOnCloudinary(req.file.path);
-  //   if (!uploadResponse) {
-  //     throw new ApiError(500, "Failed to upload image");
-  //   }
-  //   image = uploadResponse.secure_url; // Save only the secure URL or other required field
-  // } else {
-  //   throw new ApiError(408, "Image is required");
-  // }
+  let image;
+  if (req.file && req.file.path) {
+    const uploadResponse = await uploadOnCloudinary(req.file.path);
+    if (!uploadResponse) {
+      throw new ApiError(500, "Failed to upload image");
+    }
+    image = uploadResponse.secure_url; // Save only the secure URL or other required field
+  } else {
+    throw new ApiError(408, "Image is required");
+  }
 
-  const post = new Post({
-    title: title,
-    content: content,
-    author: author,
-  });
+  // const post = new Post({
+  //   title: title,
+  //   content: content,
+  //   author: author,
+  // });
+  const post = {
+    body: req.body,
+    file: req.file,
+    image: image,
+  };
   if (post) {
-    throw new ApiError(400, post);
+    throw new ApiError(400, req.body);
   }
 
   if (!post) {
