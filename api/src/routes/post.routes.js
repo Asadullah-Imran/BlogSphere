a1ee6a194fdc782a177ea1970c024ec4e559ad79
Asadullah-Imran <asadullahimran19@gmail.com>
2024-08-28 00:57:02 +0600

    Refactor createPost route to use "/create" endpoint

--- api/src/routes/post.routes.js
+++ api/src/routes/post.routes.js
@@ -23,7 +23,7 @@ const router = express.Router();
 // Define routes
 // router.post("/create", createPost);
 // router.post("/create", createPost);
-router.post("/", upload.single("image"), createPost);
+router.post("/", createPost);
 // router.post("/", verifyJWT, upload.single("image"), createPost);
 router.get("/", getPosts);
 router.get("/user/:userId", getUserPosts);
