5d02aa00705e6d165885182e844231973c1a15f2
Asadullah-Imran <asadullahimran19@gmail.com>
2024-08-29 20:22:51 +0600

    Refactor createPost route to use "/create" endpoint

--- api/src/routes/post.routes.js
+++ api/src/routes/post.routes.js
@@ -21,10 +21,7 @@ import { upload } from "../middlewares/multer.middleware.js";
 const router = express.Router();
 
 // Define routes
-// router.post("/create", createPost);
-// router.post("/create", createPost);
-router.post("/", upload.single("image"), createPost);
-// router.post("/", verifyJWT, upload.single("image"), createPost);
+router.post("/", verifyJWT, upload.single("image"), createPost);
 router.get("/", getPosts);
 router.get("/user/:userId", getUserPosts);
 router.get("/:id", getPostById);
